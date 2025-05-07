c508aa5a19fe10dead3d52f81e231999a5e25819
IDGS-904-22001450 <johanlino.idgs904@gmail.com>
2025-05-06 19:35:46 -0600

    Tema3 if When

--- app/src/main/java/com/jlino/myapplication/TemasKotlin/tema2Colecciones.kt
+++ app/src/main/java/com/jlino/myapplication/TemasKotlin/tema2Colecciones.kt
@@ -9,8 +9,22 @@ package com.jlino.myapplication.TemasKotlin
 * MutableSetOf
 * */
 
+/*
 val readOnlyFiguras = listOf("cuadrado","triangulo","circulo")
 
 var mutableFiguras = mutableListOf("cuadrado","triangulo","circulo")
 
 val coches= mapOf("Uno" to 3)
+*/
+fun main(){
+    val readOnlyFiguras = listOf("cuadrado", "triangulo", "circulo")
+    println("La primera figura es ${readOnlyFiguras[0]}")
+    println("El primer elemento es ${readOnlyFiguras.first()}")
+    println("numero de elementos ${readOnlyFiguras.count()}")
+    println("circulo" in readOnlyFiguras)
+   //readOnlyFiguras .add("pentagono")
+    var figura: MutableList<String> = mutableListOf("cuadrado2","triangulo2","circulo2")
+    println(figura)
+    figura.remove("cuadrado2")
+
+}
