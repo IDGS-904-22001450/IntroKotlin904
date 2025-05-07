05556a504266d958eb5b4d16d625df180d123002
Johan Lino <johan.antonio25@gmail.com>
2025-05-06 18:40:38 -0600

    Primer commit

---
+++ app/src/main/java/com/jlino/myapplication/TemasKotlin/tema1Variables.kt
@@ -0,0 +1,26 @@
+package com.jlino.myapplication.TemasKotlin
+
+fun main(){
+
+    //Variables solo lectura val
+    val a=4
+    //Variables mutables
+    var b = 8
+    var c = 10
+
+    println(a)
+    println("El valor de b es: $b")
+    print("El valor de c+2 es: ${c+2}")
+    //Operadores ternarios
+    c+=2
+    c+=8
+    c-=5
+    c*8
+    c/=2
+    println("El valor de c: $c")
+
+   // val num:int 25
+
+
+    println("Hola mundo")
+}
---
+++ app/src/main/java/com/jlino/myapplication/TemasKotlin/tema2Colecciones.kt
@@ -0,0 +1,16 @@
+package com.jlino.myapplication.TemasKotlin
+
+/*
+* Lista
+* Map
+* MutableMap
+* MutableSet
+* SetOf
+* MutableSetOf
+* */
+
+val readOnlyFiguras = listOf("cuadrado","triangulo","circulo")
+
+var mutableFiguras = mutableListOf("cuadrado","triangulo","circulo")
+
+val coches= mapOf("Uno" to 1)
